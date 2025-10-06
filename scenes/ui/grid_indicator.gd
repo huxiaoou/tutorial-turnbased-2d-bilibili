277a4d26d8525327443003fa7273c9b3dd48b76b
huxiaoou <516984451@qq.com>
2025-10-06 22:00:10 +0800

    Fix a warning when delta is not used in _process for grid_indicator.gd

--- scenes/ui/grid_indicator.gd
+++ scenes/ui/grid_indicator.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 var mouse_grid_position: Vector2i
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	var new_mouse_grid_position: Vector2i = ManagerGrid.get_mouse_grid_position()
 	if new_mouse_grid_position != mouse_grid_position:
 		mouse_grid_position = new_mouse_grid_position
